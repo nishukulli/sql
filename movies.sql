5d83afcc781ad55df15aa60bb16ea3c35f363732
nishukulli <105286121+nishukulli@users.noreply.github.com>
2022-05-10 20:37:56 +0530

    movies query

--- movies.sql
+++ movies.sql
@@ -5,4 +5,22 @@ id int, movie_name varchar(45),released int,director_name varchar(30),hero_name 
 select *
 from movies;
 
-insert into movies values(2,'jo;;y days',2010,'abcd','santu')
+insert into movies values(2,'jo;;y days',2010,'abcd','santu');
+insert into movies values(3,'manikya',2014,'kumar','kiccha sudeep');
+insert into movies values(4,'baahubali',2015,'rajmouli','prabhas');
+insert into movies values(5,'james',2022,'chethan','puneeth rajkumar');
+insert into movies values(6,'ninnindale',2014,'jayanth','puneeth rajkumar');
+insert into movies values(7,'yuvarathna',2021,'vijay','puneeth rajkumar');
+insert into movies values(8,'RRR',2022,'rajmouli','N T ram rao and ram charan');
+insert into movies values(9,'madhagaja',2021,'mahesh kumar','sri muruli');
+insert into movies values(10,'pushpa',2021,'sukumar','allu arjun');
+insert into movies values(11,'googly',2013,'pawan','yash');
+insert into movies values(12,'kotigobba3',2021,'shiva karthik','sudeep');
+insert into movies values(13,'milana',2007,'mano murthy','puneeth rajkumar');
+insert into movies values(14,'raja huli',2013,'guru deshpande','yash');
+insert into movies values(15,'sakath',2021,'suni','ganesh');
+insert into movies values(16,'love mocktail',2020,'darling krishna','darling krishna');
+insert into movies values(17,'dia',2020,'ashok','pruthvi');
+insert into movies values(18,'by two love',2022,'hari santu','dhanveer');
+insert into movies values(19,'ugramm',2014,'prashanth','sri muruli');
+insert into movies values(20,'pogaru',2021,'nanda kishore','dhruva sarja');
