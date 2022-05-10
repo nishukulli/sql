2ddb8a5ef750230d9280e2c24ddeff800fff722c
nishukulli <105286121+nishukulli@users.noreply.github.com>
2022-05-10 16:55:44 +0530

    movies

---
+++ movies.sql
@@ -0,0 +1,8 @@
+use nisarga;
+create table movies(
+id int, movie_name varchar(45),released int,director_name varchar(30),hero_name varchar(30));
+
+select *
+from movies;
+
+insert into movies values(2,'jo;;y days',2010,'abcd','santu')
