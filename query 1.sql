5e80c62b2e43b64304d948576216cd6edec1b640
nishukulli <105286121+nishukulli@users.noreply.github.com>
2022-05-11 13:06:30 +0530

    songs updated queries

--- query 1.sql
+++ query 1.sql
@@ -4,4 +4,12 @@ create table songs(id int,song_name varchar(45),singer varchar(45),album_name va
 select *
 from songs;
 
-insert into songs values(2,)
+insert into songs values(2,'kannamaniye','sanjith hegde','pailwaan',2019);
+insert into songs values(3,'sanchariyagu nee','vijay prakash','love mocktail',2022);
+insert into songs values(4,'halli naatu','rahul','RRR',2022);
+insert into songs values(5,'marali manasagide','sanjith hegde','gentleman',2020);
+insert into songs values(6,'mellane','sanjith hegde','rider',2022);
+insert into songs values(7,'yenammi yenammi','vijay prakash','ayogya',2019);
+insert into songs values(8,'summane','shreya ghoshal','amar',2019);
+insert into songs values(9,'soul of dia','chinmayi','dia',2020);
+insert into songs values(10,'he sharade','vasuki vaibhav','sarkari hi.pra.shaale',2018);
