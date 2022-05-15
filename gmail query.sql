f3eb3057c91c04a761aa9e65805e6216cdf0c74f
nishukulli <105286121+nishukulli@users.noreply.github.com>
2022-05-15 20:40:35 +0530

    Add files via upload
    gmailquery

---
+++ gmail query.sql
@@ -0,0 +1,18 @@
+use nisarga;
+create table gmail(
+ id int,first_name varchar(45),laast_name varchar(45),age int,mob_num int,user_name varchar(45),password varchar(45));
+ 
+ select *
+ from gmail;s
+ 
+ insert into gmail values(1,'aishwarya','d',20,9087654321,'daishwarya','abcd');
+ insert into gmail values(2,'bharath','a',25,9123456780,'abharath','dcba');
+ insert into gmail values(3,'vidya','c',21,8091234567,'cvidya','efgh');
+ insert into gmail values(4,'teju','k',23,7654321908,'kteju','hgfe');
+ insert into gmail values(5,'nisarga','bs',24,9148639422,'bsnisarga','gfeh');
+ insert into gmail values(6,'vijay','s',25,7848339009,'svijay','mnop');
+ insert into gmail values(7,'chethan','g',26,9980909587,'gchethan','xyz');
+ insert into gmail values(8,'sowmya','b',27,7859090899,'bsowmya','zyx');
+ insert into gmail values(9,'megha','g',22,8759090899,'gmegha','oprq');
+ insert into gmail values(10,'akshu','s',23,9090899578,'sakshu','rqpo');
+ 
